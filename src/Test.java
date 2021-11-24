af131722f03ff6e4224411752aba9e8ad122db81
Ghita99 <94065766+Ghita99@users.noreply.github.com>
2021-11-24 19:54:28 +0100

    Delete src directory

--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String args[]){
-        Fenêtre Fenêtre = new Fenêtre("Ma fenêtre");
-        Fenêtre.setSize(600,400);
-        Fenêtre.setVisible(true);
-    }
-}
